951396f46c106650be9d9d6058f5bb7b7395797d
MINSK\Nikita_Reshetnikov <nikita_reshetnikov@epam.com>
2018-06-11 15:34:15 +0300

    Added simple logic for todo list

--- src/app/todo-list/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list/todo-list.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  public todoItems: any[] = [
+    {
+      id: 1,
+      title: 'Todo item #1'
+    },
+    {
+      id: 2,
+      title: 'Todo item #2'
+    },
+    {
+      id: 3,
+      title: 'Todo item #3'
+    },
+    {
+      id: 4,
+      title: 'Todo item #4'
+    },
+    {
+      id: 5,
+      title: 'Todo item #5'
+    }
+  ];
 
   constructor() { }
 
