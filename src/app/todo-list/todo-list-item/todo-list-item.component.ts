21c9a8a68819810f8cf36ddd9273dd96e46b431b
MINSK\Nikita_Reshetnikov <nikita_reshetnikov@epam.com>
2018-06-11 15:45:00 +0300

    Added todo-list item interface Added binding

--- src/app/todo-list/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list/todo-list-item/todo-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TodoListItem } from '../todo-list-item.model';
 
 @Component({
   selector: 'app-todo-list-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list-item.component.css']
 })
 export class TodoListItemComponent implements OnInit {
+  @Input() item: TodoListItem;
 
   constructor() { }
 
